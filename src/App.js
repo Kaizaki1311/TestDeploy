e03a3ef30842365d2893f590cfa4562a6ee21024
Kaizaki1311 <Edgar.CP1311@gmail.com>
2023-08-02 20:19:28 -0600

    Prueba3

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>src/App.js</code>
           Prueba de desploy de Proyecto React en Git Pages
         </p>
         <a
